03810eee05e92b29612bb5f7c3ee4a0c995dc845
Sam Lubrano <samuellubrano@gmail.com>
2023-12-01 15:33:46 +0000

    complete task 1 with no lan

--- routing-aid.py
+++ routing-aid.py
@@ -2,7 +2,7 @@
 
 import sys
 from optparse import OptionParser, OptionValueError
-import networkx
+import networkx as nx
 
 # TODO: optionally, add the description of your strategy for stage 3 here. 
 # <a few sentences>
@@ -10,6 +10,64 @@ import networkx
 ########
 # Main #
 ########
+
+def compute_paths(igp_filename: str, pairs_filename: str, lans_filename: str = None):
+    igp_network, id_labels = generate_graph(igp_filename)
+    pairs = get_pairs(pairs_filename)
+    shortest_paths = {
+        pair_id: nx.all_shortest_paths(igp_network, id_labels[pair[0]], id_labels[pair[1]], weight="weight") for pair_id, pair in pairs.items()
+    }
+    for pair_id, paths in shortest_paths.items():
+        output_path(pair_id, paths, id_labels)
+
+def output_path(pair_id: str, paths: list[list[int]], id_labels: dict) -> None:
+    paths = "; ".join(format_path(path, id_labels) for path in paths)
+    print(f"{pair_id}:", paths)
+
+def format_path(path: list[int], id_labels):
+    return " -> ".join(map(lambda node: id_labels[node], path))
+
+def get_pairs(filename: str) -> dict[str: tuple[str, str]]:
+    pairs = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            if line == "\n":
+                continue
+            pair_id = line.split(":")[0]
+            src_label = line.split(" ")[1]
+            dest_label = line.split(" ")[2].strip()
+            pairs[pair_id] = (src_label, dest_label)
+        return pairs
+
+def generate_graph(filename: str) -> nx.Graph:
+    with open(filename, 'r') as f:
+       lines = f.read().splitlines()
+    
+    graph = nx.DiGraph()
+    id_labels = {}
+    node_count = int(lines[0].split(" ")[1])
+    for node_line in lines[2:node_count+2]:
+        id = int(node_line.split(" ")[0])
+        label = node_line.split(" ")[1]
+        id_labels[id] = label
+        id_labels[label] = id
+        graph.add_node(id, label=label)
+    
+    edge_start_line_index = node_count+3
+    edge_count = int(lines[edge_start_line_index].split(" ")[1])
+    for edge_line in lines[edge_start_line_index+2:edge_start_line_index+edge_count+2]:
+        edge_data = edge_line.split(" ")
+        label = edge_data[0]
+        src = int(edge_data[1])
+        dest = int(edge_data[2])
+        weight = int(edge_data[3])
+        bw = int(edge_data[4])
+        delay = int(edge_data[5])
+
+        graph.add_edge(src, dest, weight=weight, bw=bw, delay=delay)
+    
+    return graph, id_labels
+
 
 if __name__ == "__main__":
     # setup CLI parser
@@ -46,6 +104,7 @@ if __name__ == "__main__":
 
     # TODO
     # <your code>
-
+    if command == "compute-paths":
+        compute_paths(igpfile, pathsfile, lansfile)
 
 
